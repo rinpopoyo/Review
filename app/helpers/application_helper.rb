30d8a34ad553ea10922cdc7de212d9cb3ca6bf3d
rinpopoyo <rinpopoyo@gmail.com>
2016-04-17 11:13:53 +0900

    Gravatar, Notification and Transactional Emails ( Gravatar )

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "https://www.gravatar.com/avatar/#{gravatar_id}"
+    #"https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
+    #  .jpg?d=identical&s=150 の箇所の意味がわからないため、記載していないが現状うまく動く。
+  end
 end
