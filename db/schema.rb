ac6e3e75cd202aee5b8a49e215cfd239df0f4edf
rinpopoyo <rinpopoyo@gmail.com>
2016-04-21 09:06:01 +0900

    Facebook Authentication ( Create User Info Page )

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160419132931) do
+ActiveRecord::Schema.define(version: 20160420234951) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 20160419132931) do
     t.string   "provider"
     t.string   "uid"
     t.string   "image"
+    t.string   "phone_number"
+    t.text     "description"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
